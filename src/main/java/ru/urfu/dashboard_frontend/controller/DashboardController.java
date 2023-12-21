20361abea336ad7512073350f778cb1e7879d7e0
Koval Dmitriy <D.Koval@hostco.ru>
2023-12-21 12:00:42 +0500

    Добавить подсветку пользователей, у которых скоро др

--- src/main/java/ru/urfu/dashboard_frontend/controller/DashboardController.java
+++ src/main/java/ru/urfu/dashboard_frontend/controller/DashboardController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,13 +17,17 @@ import ru.urfu.dashboard_frontend.dto.UserDto;
 import ru.urfu.dashboard_frontend.feign.BackendClient;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
+import java.util.function.Predicate;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class DashboardController {
   private final BackendClient backendClient;
+
+  private static final long DAYS_BEFORE_BIRTHDAY = 2L;
 
   @GetMapping("/")
   public String mainPage(Model model) {
@@ -33,8 +38,41 @@ public class DashboardController {
             .lastname("Doe")
             .build());
     model.addAttribute("user", userDto);
-    List<UserDto> users = backendClient.getAllUser().getBody();
-    model.addAttribute("users", users);
+    //Пользователи, чей день рождения уже прошел
+    List<UserDto> birthDaysNotComingUsers = new ArrayList<>();
+    //Пользователи, чей день рождения скоро наступит
+    List<UserDto> birthDaysComingUser = backendClient.getAllUser().getBody().stream()
+        .map(user -> {
+          LocalDate dateYearOfBirthEmployee = LocalDate.now()
+              .minusYears(LocalDate.now().getYear() - user.getBirthdate().getYear());
+          long diffDays = ChronoUnit.DAYS.between(dateYearOfBirthEmployee, user.getBirthdate());
+          if (diffDays > DAYS_BEFORE_BIRTHDAY || diffDays < 0L) {
+            birthDaysNotComingUsers.add(user);
+          }
+          return user;
+        })
+        .filter(user -> ChronoUnit.DAYS.between(
+            LocalDate.now().minusYears(
+                LocalDate.now().getYear() - user.getBirthdate().getYear()),
+            user.getBirthdate()) <= DAYS_BEFORE_BIRTHDAY)
+        .filter(user -> ChronoUnit.DAYS.between(
+            LocalDate.now().minusYears(
+                LocalDate.now().getYear() - user.getBirthdate().getYear()),
+            user.getBirthdate()) >= 0L)
+        .map(user -> {
+          LocalDate dateYearOfBirthEmployee = LocalDate.now().minusYears(
+              LocalDate.now().getYear() - user.getBirthdate().getYear());
+          long diffDays = ChronoUnit.DAYS.between(user.getBirthdate(), dateYearOfBirthEmployee);
+          if (diffDays <= DAYS_BEFORE_BIRTHDAY) {
+            user.setAbout("Cкоро день рождения!");
+          }
+          if (diffDays == 0L) {
+            user.setAbout("С днем рождения!");
+          }
+          return user;
+        }).toList();
+    model.addAttribute("usersBirthday", birthDaysComingUser);
+    model.addAttribute("users", birthDaysNotComingUsers);
     var notifics = backendClient.getNotificationsByUserTag(userDto.getTag()).getBody();
     Collections.reverse(notifics);
     model.addAttribute("notifics", notifics);
@@ -129,10 +167,10 @@ public class DashboardController {
                             Model model) {
     List<String> userTags = Arrays.asList(users);
     userTags.forEach(tag -> backendClient.createMessage(MessageDto.builder()
-            .userTo(UserDto.builder().tag(SecurityContextHolder.getContext().getAuthentication().getName()).build())
-            .userFrom(UserDto.builder().tag(tag).build())
-            .theme(theme)
-            .content(content)
+        .userFrom(UserDto.builder().tag(SecurityContextHolder.getContext().getAuthentication().getName()).build())
+        .userTo(UserDto.builder().tag(tag).build())
+        .theme(theme)
+        .content(content)
         .build()));
 
     UserDto userDto = Optional.ofNullable(backendClient.getMe())
